91ececa96749c50ec139af917769b20ca7fb4585
Alkio, Antton <antton.alkio@if.fi>
2021-12-01 09:21:46 +0200

    solved day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+
+void Part1()
+{
+    var increased = 0;
+
+    for (int i = 1; i < input.Length; i++)
+    {
+        if (input[i] > input[i - 1])
+        {
+            increased++;
+        }
+    }
+
+    Console.WriteLine(increased);
+}
+
+void Part2()
+{
+    var increased = 0;
+    var chunkCount = 3;
+    var sums = Enumerable.Range(0, input.Length - chunkCount + 1).Select(i => input.Skip(i).Take(chunkCount)).Select(l => l.Sum()).ToArray();
+
+    for (int i = 1; i < sums.Length; i++)
+    {
+        if (sums[i] > sums[i - 1])
+        {
+            increased++;
+        }
+    }
+
+    Console.WriteLine(increased);
+}
+
+Part1();
+Part2();
