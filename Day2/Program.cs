75210e57c5ebf6a8d4da1af78e2562e62fc45336
Alkio, Antton <antton.alkio@if.fi>
2021-12-02 09:52:31 +0200

    day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines("input.txt").Select(ParseInstruction).ToArray();
+
+void Part1()
+{
+    Position pos = new(0, 0, 0);
+
+    foreach(var instruction in input)
+    {
+        pos = instruction switch
+        {
+            Instruction i when i.Direction == "forward" => pos with { Horizontal = pos.Horizontal + i.Units },
+            Instruction i when i.Direction == "down" => pos with { Depth = pos.Depth + i.Units },
+            Instruction i when i.Direction == "up" => pos with { Depth = pos.Depth - i.Units }
+        };
+    }
+
+    Console.WriteLine(pos.Horizontal * pos.Depth);
+}
+
+void Part2()
+{
+    Position pos = new(0, 0, 0);
+
+    foreach (var instruction in input)
+    {
+        pos = instruction switch
+        {
+            Instruction i when i.Direction == "forward" => pos with { Horizontal = pos.Horizontal + i.Units, Depth = pos.Depth + (pos.Aim * i.Units) },
+            Instruction i when i.Direction == "down" => pos with { Aim = pos.Aim + i.Units },
+            Instruction i when i.Direction == "up" => pos with { Aim = pos.Aim - i.Units }
+        };
+    }
+
+    Console.WriteLine(pos.Horizontal * pos.Depth);
+}
+
+Part1();
+Part2();
+
+
+static Instruction ParseInstruction(string str) => new Instruction(str.Split(' ')[0], int.Parse(str.Split(' ')[1]));
+
+record Instruction(string Direction, int Units);
+
+record Position(int Horizontal, int Depth, int Aim);
